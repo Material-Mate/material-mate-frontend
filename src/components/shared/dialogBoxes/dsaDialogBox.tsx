20259654f152c20312c72da72d007970c5fb401e
akbar <pathank444@gmail.com>
2025-02-19 20:00:54 +0530

    added

---
+++ src/components/shared/dialogBoxes/dsaDialogBox.tsx
@@ -0,0 +1,78 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Download, Play } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+const DsaDialogBox = ({
+  activeSubject,
+  isOpen,
+  onChange,
+}: {
+  activeSubject:CSSubject
+  isOpen: boolean;
+  onChange: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent className="max-w-[90%] sm:max-w-[60%] md:max-w-[50%] lg:max-w-[30%] h-auto  bg-zinc-950 flex flex-col">
+        <Image
+          src={
+            activeSubject.thumbnail
+          }
+          height={400}
+          width={400}
+          alt=""
+          className="rounded-xl"
+        />
+      <div className="w-full h-full">
+      <DialogHeader className="h-[10%]">
+          <DialogTitle className="text-2xl">
+            <span className="bg-gradient-to-r from-purple-300 to-purple-400 bg-clip-text text-transparent">
+             {activeSubject.name}
+            </span>
+          </DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col gap-4 w-full max-h-[15rem]  min-h-[10rem] overflow-y-auto px-2 py-2">
+          {activeSubject.videos_links.map((lecture, index) => (
+            <div
+              key={index}
+              className="w-full py-3 rounded-lg border border-slate-400 flex justify-between items-center px-5"
+            >
+              <p>Lecture {index+1}: {lecture.lecture_name}</p>
+             
+             <Link href={lecture.video_link} className="p-2 font- cursor-pointer hover:scale-95 hover:bg-purple-500 border rounded-full"
+             >
+             <Play size={15} className="font-semibold"/>
+             </Link>
+            </div>
+          ))}
+        </div>
+        <DialogFooter className="w-full flex">
+          <Button
+            onClick={() => onChange(false)}
+            className="bg-gradient-to-r mt-2 w-1/2"
+          >
+            Close
+          </Button>
+          <Button
+            className="flex gap-1 items-center bg-gradient-to-r mt-2 w-1/2 from-purple-400 to-purple-600"
+          >
+            <Link href={activeSubject.notes_link} className="flex gap-1 items-center">
+            Practise Session
+            </Link>
+           
+          </Button>
+        </DialogFooter>
+      </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DsaDialogBox;
--- src/data/dummy.ts
+++ src/data/dummy.ts
@@ -144,3 +144,206 @@ export const web_development:WebDevelpoment[] = [
 export const android_development:AndroidDevelopment[] = [
   
 ]
+
+export const DSA:CSSubject[] = [
+  {
+    name:"Array",
+    thumbnail:"https://i.ytimg.com/vi/n60Dn0UsbEk/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLDQ5qLK6D6qAZo2A3saw3LdDkYeDQ",
+    discription:"Welcome to the ultimate guide to mastering Operating Systems! In this comprehensive 16-hour video",
+    total_video_count:1,
+    videos_links :[
+      {
+        lecture_name: "Array",
+        video_link: "https://youtu.be/n60Dn0UsbEk?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+      {
+        lecture_name: "linear search",
+        video_link: "https://youtu.be/_HRA37X8N_Q?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+      {
+        lecture_name: "Binary search",
+        video_link: "https://youtu.be/f6UU7V3szVw?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      }
+    ],
+    notes_link:"https://www.youtube.com/playlist?list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB"
+  },
+  {
+    name:"String",
+    thumbnail:"https://i.ytimg.com/vi/zL1DPZ0Ovlo/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLD7uWE4naTqckIrGL0mNX3IR4KPeA",
+    discription:"In this comprehensive video, we cover all the essential topics required to excel in DBMS for placements",
+    total_video_count:2,
+    videos_links :[
+      {
+        lecture_name: "Strings and its builder",
+        video_link: "https://youtu.be/zL1DPZ0Ovlo?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+      {
+        lecture_name: "Pattern questions",
+        video_link: "https://youtu.be/lsOOs5J8ycw?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+     
+    ],
+    notes_link:"https://www.youtube.com/playlist?list=PLDdcY4olLQk0A0o2U0fOUjmO2v3X6GOxX"
+  },
+  {
+    name:"OOPS",
+    thumbnail:"https://i.ytimg.com/vi/BSVKUk58K6U/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLAtgfJEGoViXhZHDBYYUGrD1vuSjQ",
+    discription:"In this comprehensive video, we cover all the essential topics required to excel in OOPs for placements",
+    total_video_count:7,
+    videos_links :[
+      {
+        lecture_name: "OOPs Part-1",
+        video_link: "https://youtu.be/BSVKUk58K6U?si=zpHs42oWjdpDUPUJ",
+      },
+      {
+        lecture_name: "OOPs Part-2",
+        video_link: "https://youtu.be/_Ya6CN13t8k?si=nVdLjDYnkrdY6ExV",
+      },
+      {
+        lecture_name: "OOPs Part-3",
+        video_link: "https://youtu.be/46T2wD3IuhM?si=mYvyO--EgTmcZldO",
+      },
+      {
+        lecture_name: "OOPs Part-4",
+        video_link: "https://youtu.be/W145DXs8fFg?si=iFPCX_Hui-M1h7Kt",
+      },
+      {
+        lecture_name: "OOPs Part-5",
+        video_link: "https://youtu.be/rgHZa7-Dibg?si=JaX8Uy6XREZSfs4U",
+      },
+      {
+        lecture_name: "OOPs Part-6",
+        video_link: "https://youtu.be/OY2lPr8h93U?si=V4eW1hTNw2Qcq1Br",
+      },
+      {
+        lecture_name: "OOPs Part-7",
+        video_link: "https://youtu.be/9ogGan-R1pc?si=P0yYWs5PpLu46N4X",
+      }
+    ],
+    notes_link:"https://www.youtube.com/playlist?list=PLbtQV2hHYdSFiwaB-9occylQtmJNTu3OY"
+  },
+  {
+    name:"Recursion",
+    thumbnail:"https://i.ytimg.com/vi/M2uO2nMT0Bk/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLD6ZvrO-qwJgGQ-XRpakcVAonFgYw",
+    discription:"In this comprehensive video, we cover all the essential topics required to excel in OOPs for placements",
+    total_video_count:3,
+    videos_links :[
+      {
+        lecture_name: "Recursion",
+        video_link: "https://youtu.be/M2uO2nMT0Bk?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+      {
+        lecture_name: "R-1",
+        video_link: "https://youtu.be/JxILxTwHukM?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+      {
+        lecture_name: "R-2",
+        video_link: "https://youtu.be/9ByWqPzfXDU?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      }
+    ],
+    notes_link:"https://drive.google.com/drive/folders/1rw9fqwNct6I5o_tJxD7_hcJiR8z0OJn9?usp=drive_link"
+  },
+  {
+    name:"Linkedlist",
+    thumbnail:"https://i.ytimg.com/vi/58YbpRDc4yw/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLCq_C8lnJ2w6GOFoRu7GP_9m8i8lg",
+    discription:"In this comprehensive video, we cover all the essential topics required to excel in CN for placements",
+    total_video_count:2,
+    videos_links :[
+      {
+        lecture_name: "linkedlist",
+        video_link: "https://youtu.be/58YbpRDc4yw?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+      {
+        lecture_name: "linkedlist-qusetion",
+        video_link: "https://youtu.be/70tx7KcMROc?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+    ],
+    notes_link:"https://www.youtube.com/watch?v=FrGKsNBaq_4&pp=ygUcbGlua2VkbGlzdCBxdXNldGlvbiBmb3IgZHNhIA%3D%3D"
+  },
+  {
+    name:"Stack & Queue",
+    thumbnail:"https://i.ytimg.com/vi/rHQI4mrJ3cg/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLBvN2nLamMWJbkpz0pbd2qxhNKnbA",
+    discription:"In this comprehensive video, we cover all the essential topics required to excel in CN for placements",
+    total_video_count:3,
+    videos_links :[
+      {
+        lecture_name: "Overview",
+        video_link: "https://youtu.be/rHQI4mrJ3cg?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+      {
+        lecture_name: "SQ Question",
+        video_link: "https://youtu.be/S9LUYztYLu4?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+    ],
+    notes_link:"https://www.youtube.com/watch?v=TiYif1O1NTc&list=PLDdcY4olLQk1bZS0OOFLAysk6keprt00U"
+  },
+  {
+    name:"Tree in dsa",
+    thumbnail:"https://i.ytimg.com/vi/4s1Tcvm00pA/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLBcNlVwRZ426tjaqy8YEixzB8gqPA",
+    discription:"In this comprehensive video, we cover all the essential topics required to excel in CN for placements",
+    total_video_count:4,
+    videos_links :[
+      {
+        lecture_name: "Binary tree",
+        video_link: "https://youtu.be/4s1Tcvm00pA?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+      {
+        lecture_name: "AVL Tree",
+        video_link: "https://youtu.be/CVA85JuJEn0?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      },
+      {
+        lecture_name: "Segment Tree",
+        video_link: "https://youtu.be/ciHThtTVNto?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ",
+      }
+    ],
+    notes_link:"https://youtu.be/9D-vP-jcc-Y?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ"
+  },
+  {
+    name:"Graph",
+    thumbnail:"https://i.ytimg.com/vi/EaK6aslcC5g/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLCTlE4gOspy1HrFT1ZUUV4zgQstHg",
+    discription:"In this comprehensive video, we cover all the essential topics required to excel in CN for placements",
+    total_video_count:3,
+    videos_links :[
+      {
+        lecture_name: "Intro",
+        video_link: "https://youtu.be/EaK6aslcC5g?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA",
+      },
+      {
+        lecture_name: "BFS",
+        video_link: "https://youtu.be/b5kij1Akf9I?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA",
+      },
+      {
+        lecture_name: "DFS",
+        video_link: "https://youtu.be/aJa3U-hydXc",
+      },
+      {
+        lecture_name: "Dijsktra Algorithm",
+        video_link: "https://youtu.be/dVUR3Rm6biE?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA",
+      },
+      {
+        lecture_name: "Kruskal's Algorithm",
+        video_link: "https://youtu.be/KxLtIrCyXwE?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA",
+      },
+
+    ],
+    notes_link:"https://www.youtube.com/watch?v=M3_pLsDdeuU&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn"
+  },
+  {
+    name:"Dynamic Programming",
+    thumbnail:"https://i.ytimg.com/vi/PGsgv6nXhLw/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLBIT-0FdnuM53Qr1m9E4paqp0oZ3Q",
+    discription:"In this comprehensive video, we cover all the essential topics required to excel in CN for placements",
+    total_video_count:4,
+    videos_links :[
+      {
+        lecture_name: "DP",
+        video_link: "https://youtu.be/PGsgv6nXhLw?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA",
+      },
+      {
+        lecture_name: "DP-Question",
+        video_link: "https://www.youtube.com/watch?v=PGsgv6nXhLw&list=PLDzeHZWIZsTomOPnCiU3J95WufjE36wsb",
+      },
+    
+    ],
+    notes_link:"https://www.youtube.com/watch?v=smTnSl6UKi0&list=PLpIkg8OmuX-L_QqcKB5abYynQbonaNcq3"
+  },
+]
