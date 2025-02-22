16ed369cf95f7cbace0a9feb712b4bc5a47d4697
Gaurav Sharma <gauravsharma16072001@gmail.com>
2025-02-22 19:00:36 +0530

    added responsive navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,24 @@ export default {
     "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      '2xl': {'min': '1535px'},
+      // => @media (min-width: 1535px) { ... }
+
+      'xl': {'min': '1279px'},
+      // => @media (min-width: 1279px) { ... }
+
+      'lg': {'min': '1023px'},
+      // => @media (min-width: 1023px) { ... }
+
+      'md': {'min': '767px'},
+      // => @media (min-width: 767px) { ... }
+
+      'sm': {'min': '639px'},
+      // => @media (min-width: 639px) { ... }
+      'xs': {'min': '200px'},
+      // => @media (min-width: 639px) { ... }
+    },
     extend: {
       animation: {
         border: 'border 4s ease infinite',
